7011e230dc0049d66fedeca8e85b740baff29e35
dattito <32016658+dattito@users.noreply.github.com>
2024-02-04 21:40:44 +0100

    feat: add more details

--- Digital-Product-Pass/src/components/supply-chain.tsx
+++ Digital-Product-Pass/src/components/supply-chain.tsx
@@ -2,21 +2,27 @@ import "./supply-chain.css";
 
 export default function SupplyChain() {
   return (
-    <div className="trip" id="litr::timeline">
-      <Place title="Al-Jubail" description="Saudi-Arabien" logo="adjust" />
-      <Transport title="Schiff" logo="directions_boat" co2e="10-20" />
-      <Place title="Guangzhou" description="China" logo="adjust" />
-      <Transport title="Schiff" logo="directions_boat" co2e="10-20" />
-      <Transport title="LKW" logo="local_shipping" co2e="60-150" />
-      <Place title="Stuttgart" description="Deutschland" logo="adjust" />
-      <Transport title="LKW" logo="local_shipping" co2e="60-150" />
-      <Place
-        title="Mannheim"
-        description="Deutschland (Verkauf)"
-        logo="storefront"
-      />
-      <div className="bar"></div>
-    </div>
+    <>
+      <div className="trip" id="litr::timeline">
+        <Place title="Al-Jubail" description="Saudi-Arabien" logo="adjust" />
+        <Transport title="Schiff" logo="directions_boat" co2e="10-20" />
+        <Place title="Guangzhou" description="China" logo="adjust" />
+        <Transport title="Schiff" logo="directions_boat" co2e="10-20" />
+        <Transport title="LKW" logo="local_shipping" co2e="60-150" />
+        <Place title="Stuttgart" description="Deutschland" logo="adjust" />
+        <Transport title="LKW" logo="local_shipping" co2e="60-150" />
+        <Place
+          title="Mannheim"
+          description="Deutschland (Verkauf)"
+          logo="storefront"
+        />
+        <div className="bar"></div>
+      </div>
+
+      <div className="text-xs text-gray-600 mt-4 mr-4 text-right">
+        *g CO₂e/tkm = Gramm CO₂-Äquivalente pro Tonne und Kilometer
+      </div>
+    </>
   );
 }
 
@@ -62,7 +68,7 @@ function Transport({
       </div>
       <div className="details">
         <div className="font-semibold">{title}</div>
-        <div className="text-xs">{co2e} g CO₂e/tkm</div>
+        <div className="text-xs">{co2e} g CO₂e/tkm*</div>
       </div>
     </div>
   );
