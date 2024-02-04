70ea6d7bb6b049204f60ee489739bd302892a789
Max Spannagel <m.spannagel@mbinary.de>
2024-02-04 08:58:36 +0100

    Add Demo project and repos

---
+++ Demo/src/components/DeliveryProcess.tsx
@@ -0,0 +1,26 @@
+// src/components/DeliveryProcess.tsx
+import React from 'react';
+
+interface DeliveryStep {
+  step: string;
+  co2Value: number;
+}
+
+interface DeliveryProcessProps {
+  steps: DeliveryStep[];
+}
+
+const DeliveryProcess: React.FC<DeliveryProcessProps> = ({ steps }) => {
+  return (
+    <div>
+      <h2>Delivery Process</h2>
+      <ul>
+        {steps.map((step, index) => (
+          <li key={index}>{`${step.step}: ${step.co2Value} CO2`}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DeliveryProcess;
